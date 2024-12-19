5fdd346e71e43daac1f7a75df63dde6400cee93e
Gobinda Gagan <gobindagagan@gmail.com>
2024-12-19 23:09:43 +0530

    mailSender code add pre middleware

---
+++ Server/Controllers/Auth.js
@@ -0,0 +1,4 @@
+//sendOTP
+//logIn
+//SignUP
+// changePassword
--- Server/Models/OTP.js
+++ Server/Models/OTP.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const mailSender = require("../Utils/mailSender");
 
-const otpSchema = new mongoose.Schema({
+const OTPSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -16,4 +17,25 @@ const otpSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("OTP", otpSchema);
+// a function ->to send emails
+
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "verification Email from StudyNotion",
+      otp
+    );
+    console.log("Verification email sent successfully");
+  } catch (error) {
+    console.log("error sending verification email", error);
+    throw error;
+  }
+}
+
+OTPSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+});
+
+module.exports = mongoose.model("OTP", OTPSchema);
